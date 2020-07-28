eced2b163b330b8b0cebf16993e06a2bcb2a3444
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-28 12:34:15 -0400

    typewriter.js edit sytnax

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,11 @@
-const { nextTick } = require("process");
-const { doesNotMatch } = require("assert");
-
 const sentence = "hello there from lighthouse labs";
-let count = 0
+let count = 0;
 
 for (const char in sentence) {
-  count = char * 50
+  count = char * 50;
   setTimeout(() => {
-    process.stdout.write(sentence[char])}, char * 50)
-};
-  setTimeout(() => process.stdout.write("\n"), count);
+    process.stdout.write(sentence[char]);
+  }, char * 50);
+}
+setTimeout(() => process.stdout.write("\n"), count);
 
