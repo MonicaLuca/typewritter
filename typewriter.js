f8ef11f465fc25419111e5c98ae9a2da5a52fa0f
MonicaLuca <monica.lucarini28@gmail.com>
2020-07-28 12:31:58 -0400

    typewriter.js added

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const { nextTick } = require("process");
+const { doesNotMatch } = require("assert");
+
+const sentence = "hello there from lighthouse labs";
+let count = 0
+
+for (const char in sentence) {
+  count = char * 50
+  setTimeout(() => {
+    process.stdout.write(sentence[char])}, char * 50)
+};
+  setTimeout(() => process.stdout.write("\n"), count);
+
